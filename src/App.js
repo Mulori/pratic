c3c0dd8ec58cc8f0ea7ee866a718122faf9eb30f
Mulori <murilogarcia.hr@gmail.com>
2022-11-15 00:37:43 -0300

    updarte

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
-import FormLogin from './login';
+import Home from './pages/home';
+import FormLogin from './pages/login';
+import FormRegister from './pages/register';
 
 function App() {
   return (
-    <div className="App">      
-      <div className="Conteudo"> 
-        <FormLogin />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<FormLogin />} />
+        <Route path="/register" element={<FormRegister />} />
+      </Routes>
+    </Router>
   );
 }
 
